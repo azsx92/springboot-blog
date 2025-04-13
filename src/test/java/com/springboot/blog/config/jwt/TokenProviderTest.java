95e46787c6ab38ee1d060fa081c2caee89ab6d48
azsx92 <azsx92@gmail.com>
2025-04-13 18:54:30 +0900

    feat: 9 jwt getUserId test

--- src/test/java/com/springboot/blog/config/jwt/TokenProviderTest.java
+++ src/test/java/com/springboot/blog/config/jwt/TokenProviderTest.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Duration;
 import java.util.Date;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -79,7 +80,7 @@ class TokenProviderTest {
         assertThat(result).isTrue();
     }
 
-    //1 . getAuthentication 검증 테스트
+    //3 . getAuthentication 검증 테스트
     @DisplayName("getAuthentication() : 토큰 기반으로 인증 정보를 가져올 수 있다.")
     @Test
     void getAuthentication() {
@@ -95,8 +96,23 @@ class TokenProviderTest {
         // then
         assertThat(((UserDetails) authentication.getPrincipal()).getUsername()).isEqualTo(userEmail);
     }
-
+    //4 . getUserId 검증 테스트
+    @DisplayName("getUserId() : 토큰 으로 유저 ID를 가져올 수 있다.")
     @Test
     void getUserId() {
+        // given
+        Long userId = 1L;
+        String token = JwtFactory.builder()
+                .claims(Map.of("id", userId))
+                .build()
+                .createToken(jwtProperties);
+
+
+        // when
+        Long userIdByToken = tokenProvider.getUserId(token);
+
+        // then
+        assertThat(userIdByToken).isEqualTo(userId);
     }
+
 }
